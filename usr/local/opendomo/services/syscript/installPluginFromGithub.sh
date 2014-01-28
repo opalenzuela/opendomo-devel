87321d1c753d9128ae49a55af5706bbbf14905e4
opalenzuela <opalenzuela@gmail.com>
2014-01-28 15:33:52 +0100

    First approach with the visual interface

--- usr/local/opendomo/services/syscript/installPluginFromGithub.sh
+++ usr/local/opendomo/services/syscript/installPluginFromGithub.sh
@@ -3,5 +3,20 @@
 #type:local
 #package:oddevel
 
-echo "# This script is under development"
+
+if test -z "$1"
+then
+	echo "#>Install plugin from Github"
+	echo "form:`basename $0`"
+	echo "	user	User	text"
+	echo "	project	Project	text"
+else
+	if /usr/local/opendomo/bin/plugin_add_from_gh.sh $1 $2 >/dev/null
+	then
+		echo "# Installing plugin"
+	else
+		echo "#ERROR The plugin was not found"	
+	fi
+fi
+echo
 echo
