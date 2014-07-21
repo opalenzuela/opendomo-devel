35be277341f861b00c64727f288364dd0431746d
opalenzuela <opalenzuela@gmail.com>
2014-07-21 11:46:31 +0200

    adding common script for CGI

---
+++ var/www/scripts/common/developerbar.js
@@ -0,0 +1,5 @@
+
+jQuery(function($) {
+	var developerBar="<div id='developerbar'><a id='makeawish' href='https://github.com/opalenzuela/opendomo/issues?state=open' target='makeawish'>Make a wish!</a></div>";
+	$("#footer").prepend(developerBar);
+});
