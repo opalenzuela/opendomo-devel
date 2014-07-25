cc4d4e4d2d0f2bc4e322bf4f1a45aba95ef752f8
opalenzuela <opalenzuela@gmail.com>
2014-07-25 09:57:19 +0200

    Toolbar for developers

--- var/www/scripts/common/developerbar.js
+++ var/www/scripts/common/developerbar.js
@@ -1,5 +1,8 @@
 
 jQuery(function($) {
-	var developerBar="<div id='developerbar'><a id='makeawish' href='https://github.com/opalenzuela/opendomo/issues?state=open' target='makeawish'>Make a wish!</a></div>";
+	var developerBar="<a id='makeawish' href='http://es.opendomo.org/makeawish' target='makeawish'></a>";
+	developerBar = developerBar + "<a id='debugbutton' href='javascript:showDebug();'></a>";
 	$("#footer").prepend(developerBar);
+	$("#makeawish").css("display","inline-block").css("background","url(/images/wish.png) no-repeat scroll center center").css("height","30px").css("width","30px")
+	$("#debugbutton").css("display","inline-block").css("background","url(/images/debug.png) no-repeat scroll center center").css("height","30px").css("width","30px")
 });
