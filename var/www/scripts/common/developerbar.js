7e3aff53a4fd1d1281cda3912c0f144050817352
opalenzuela <opalenzuela@gmail.com>
2014-07-25 10:51:01 +0200

    Unifying styles

--- var/www/scripts/common/developerbar.js
+++ var/www/scripts/common/developerbar.js
@@ -1,10 +1,10 @@
 
 jQuery(function($) {
-	var developerBar="<a id='makeawish' href='http://es.opendomo.org/makeawish' target='makeawish'></a>";
-	developerBar = developerBar + "<a id='debugbutton' href='javascript:showDebug();'></a>";
-	developerBar = developerBar + "<a id='notify' href='javascript:notifyProblem();'></a>";
-	developerBar = developerBar + "<a id='translate' href='javascript:putFlags();'></a>";
-	$("#footer").prepend(developerBar);
+	var    dbar = "<a id='makeawish' class='tool' href='http://es.opendomo.org/makeawish' target='makeawish'></a>";
+	dbar = dbar + "<a id='debugbutton' class='tool' href='javascript:showDebug();'></a>";
+	dbar = dbar + "<a id='notify' class='tool' href='javascript:notifyProblem();'></a>";
+	dbar = dbar + "<a id='translate' class='tool' href='javascript:putFlags();'></a>";
+	$("#footer").prepend(dbar);
 	$("#makeawish").css("display","inline-block").css("background","url(/images/wish.png) no-repeat scroll center center").css("height","32px").css("width","32px");
 	$("#debugbutton").css("display","inline-block").css("background","url(/images/debug.png) no-repeat scroll center center").css("height","32px").css("width","32px");
 	$("#notify").css("display","inline-block").css("background","url(/images/notify.png) no-repeat scroll center center").css("height","32px").css("width","32px");
