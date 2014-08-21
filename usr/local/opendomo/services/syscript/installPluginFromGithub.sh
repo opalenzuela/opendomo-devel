3338b9a73814ba58b938d442a7df0deef7a52cf2
opalenzuela <opalenzuela@gmail.com>
2014-08-21 09:37:07 +0200

    Improving GUI

--- usr/local/opendomo/services/syscript/installPluginFromGithub.sh
+++ usr/local/opendomo/services/syscript/installPluginFromGithub.sh
@@ -4,18 +4,19 @@
 #package:oddevel
 
 
-if test -z "$1"
+if test -z "$2"
 then
 	echo "#>Install plugin from Github"
 	echo "form:`basename $0`"
-	echo "	user	User	text"
-	echo "	project	Project	text"
+	echo "	user	User	text	$1"
+	echo "	project	Project	text	$2" 
 else
+	echo "#LOADING Installing plugin"
 	if /usr/local/opendomo/bin/plugin_add_from_gh.sh $1 $2 >/dev/null
 	then
-		echo "# Installing plugin"
+		echo "#INFO Plugin [$2] installed"
 	else
-		echo "#ERROR The plugin was not found"	
+		echo "#ERRO The plugin was not found"	
 	fi
 fi
 echo
