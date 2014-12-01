db464b65c2a086781e21c0c154bef2b0d2f17cb2
opalenzuela <opalenzuela@gmail.com>
2014-12-01 10:51:53 +0100

    Revert "Self-documented and self-explanatori"
    This reverts commit d5726bd857027298c47cc349ad6f154887cd0e37.

--- usr/local/opendomo/services/tools/developerManual.sh
+++ usr/local/opendomo/services/tools/developerManual.sh
@@ -3,15 +3,7 @@
 #package:oddevel
 
 # Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
-
-## This service will display the manual page corresponding to the 
-## requested service.
-
 echo "#>Developer manual"
-if test -z "$1"; then
-	echo "list:$0"
-	echo "	-intro	Introduction	chapter"
-else
-	cat /usr/local/opendomo/docs/$1.txt | sed 's/^/# /'
-fi
+echo "list:$0"
+echo "	-intro	Introduction	chapter"
 echo
