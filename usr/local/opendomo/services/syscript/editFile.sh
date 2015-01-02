538f7a2732274180233442caa88b80675cf98090
opalenzuela <opalenzuela@gmail.com>
2015-01-02 10:03:14 +0100

    Trying to display images as well

--- usr/local/opendomo/services/syscript/editFile.sh
+++ usr/local/opendomo/services/syscript/editFile.sh
@@ -4,10 +4,19 @@
 
 # Copyright(c) 2015 OpenDomo Services SL. Licensed under GPL v3 or later
 
+extension=${1##*.}
+
 echo "#>Editing file"
 echo "form:editFile.sh"
 echo "	fullpath	Filename	readonly	$1"
-echo "	filecontent	File contents 	application	/cgi-bin/texteditor.py?fname=$1"
+case $extension in
+	jpeg|jpg|png|gif)
+		echo "	filecontent	File contents 	application	/cgi-bin/displayfile.cgi?$1"
+		;;
+	*)
+		echo "	filecontent	File contents 	application	/cgi-bin/texteditor.py?fname=$1"
+	
+esac
 echo "actions:"
 echo "	goback	Back"
 echo 
---
+++ var/www/cgi-bin/displayfile.cgi
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Content-type:image/jpeg"
+echo 
+cat /var/opendomo/tmp/$CGI_SCRIPT_NAME
