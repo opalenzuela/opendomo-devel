6045a65bd88021ec1300d25b456fc7c09948a129
opalenzuela <opalenzuela@gmail.com>
2015-01-02 09:08:19 +0100

    Preparing for file edition

--- usr/local/opendomo/services/syscript/editFile.sh
+++ usr/local/opendomo/services/syscript/editFile.sh
@@ -4,8 +4,10 @@
 
 # Copyright(c) 2015 OpenDomo Services SL. Licensed under GPL v3 or later
 
-echo "#>Editing file [$1]"
+echo "#>Editing file"
 echo "form:editFile.sh"
+echo "	fullpath	Filename	readonly	$1"
+echo "	filecontent	File contents 	application	$1"
 echo "actions:"
 echo "	goback	Back"
 echo 
