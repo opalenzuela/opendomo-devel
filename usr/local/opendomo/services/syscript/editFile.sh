3a8082064e8b453cff280645ec9e5820b8400ba5
opalenzuela <opalenzuela@gmail.com>
2015-01-05 11:00:05 +0100

    Adapted to the usual file posting method

--- usr/local/opendomo/services/syscript/editFile.sh
+++ usr/local/opendomo/services/syscript/editFile.sh
@@ -5,14 +5,23 @@
 # Copyright(c) 2015 OpenDomo Services SL. Licensed under GPL v3 or later
 
 extension=${1##*.}
+TMPDIR="/var/opendomo/tmp"
+
+if ! test -z "$3"; then
+	bname=`basename $1`
+	cp $TMPDIR/$bname $TMPDIR/$1
+fi
 
 echo "#>Editing file"
 echo "form:editFile.sh"
 echo "	fullpath	Filename	readonly	$1"
 case $extension in
 	jpeg|jpg|png|gif)
+		bname=`basename $1`
+		cp $TMPDIR/$1 $TMPDIR/$bname
+		echo "	fullpath	fullpath	hidden	$1"
 		echo "	filecontent	File contents 	application	/cgi-bin/displayfile.cgi?$1"
-		echo "	realfile	Upload file	file	$1"
+		echo "	realfile	Upload file	file	$bname"
 		echo "actions:"
 		echo "	goback	Back"
 		;;
