81ea17e8a7c03af3c1bda4441e4d1594b0ed9600
opalenzuela <opalenzuela@gmail.com>
2015-02-04 16:20:07 +0100

    Updating repository

--- var/www/scripts/editFile.sh.js
+++ var/www/scripts/editFile.sh.js
@@ -1,3 +1,4 @@
+var repositoryURL="https://github.com/opalenzuela/opendomo-devel/";
 $(function(){
 	/*if ($("form").length==0) {   // it is not the image view page
 		$("div.toolbar").hide(); // No need for complementary toolbar
